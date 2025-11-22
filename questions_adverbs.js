fcd43f100f9adc08bb6ee8d40afac05b2615193c
ronaldroi2008-droid <ronaldroi2008@gmail.com>
2025-11-22 18:59:55 +0800

    Update questions_adverbs.js

--- questions_adverbs.js
+++ questions_adverbs.js
@@ -309,8 +309,8 @@ const ADVERB_QUESTIONS = {
       correctIndex: 0, 
       explanation: "Irregular! 'Hard' means with effort. 'Hardly' means 'almost not'. If you work with effort, you work **hard**." 
      }
-  ]
-},
+  ], 
+
 
   intermediate: [
     // --- 51-60: Good vs Well (Common Confusion) ---
