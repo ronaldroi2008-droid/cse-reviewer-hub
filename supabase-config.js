98649cca7433645a3a9152e00e12d0e2827d7f87
ronaldroi2008-droid <ronaldroi2008@gmail.com>
2026-06-01 20:09:52 +0800

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://xcdqnxqmwqebuaznnerg.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_04KxjGLPuPIUvbO8m6NdXA_5z22oZv5";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
